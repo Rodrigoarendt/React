1c83e157d59487a18c5a58ed05ca8ad1d69bbd7d
Rodrigo Arendt <rodrigo.arendt@hotmail.com>
2022-07-04 16:44:13 -0300

    Aula dia 22/06

--- aula22/src/components/PostCard/index.jsx
+++ aula22/src/components/PostCard/index.jsx
@@ -1,9 +1,11 @@
-export const PostCard = ({ title, cover, body, id }) => (
-  <div className="post">
+import './style.css';
+
+export const PostCard = ({title, cover, body, id}) => (
+    <div className="post">
     <img src={cover} alt={title} />
     <div className="post-content">
       <h1>{title}</h1>
       <p>{body}</p>
     </div>
   </div>
-);
+ );
