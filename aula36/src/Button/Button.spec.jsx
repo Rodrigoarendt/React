5ccbb578100d316afe1cb58ce6a83691524003da
Rodrigo Arendt <rodrigo.arendt@hotmail.com>
2022-07-23 22:28:59 -0300

    Testing Library

---
+++ aula36/src/Button/Button.spec.jsx
@@ -0,0 +1,49 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { Button } from '.';
+
+describe('<Button />', () => {
+  it('should render the button with the text "Load more"',() => {
+    render(<Button text="Load More"/>);
+
+    expect.assertions(1);
+
+    const button = screen.getByRole('button', {name: /load more/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should call function on button click"',() => {
+    const fn = jest.fn();
+    render(<Button text="Load More" onClick={fn} />);
+
+    const button = screen.getByRole('button', {name: /load more/i });
+
+    fireEvent.click(button);
+
+    expect(fn).toHaveBeenCalledTimes(1);
+    
+  });
+
+  it('should be disabled when disabled is true"',() => {
+    render(<Button text="Load More" disabled={true} />);
+
+    const button = screen.getByRole('button', {name: /load more/i });
+
+    expect(button).toBeDisabled();
+    
+  });
+
+  it('should be enabled when disabled is false',() => {
+    const fn = jest.fn();
+    render(<Button text="Load More" disabled={false} onClick={fn} />);
+    const button = screen.getByRole('button', {name: /load more/i });
+    expect(button).toBeEnabled();
+  });
+
+  it('should match snapshot', () => {
+    const fn = jest.fn();
+    const { container } = render(<Button text="Load More" disabled={false} onClick={fn} />);
+    expect(container).toBeEnabled();
+  });
+
+});
+
---
+++ aula36/src/templates/Home/Home.espec.jsx
@@ -0,0 +1,21 @@
+
+describe('<home />', () => {
+  it('test onde', () => {
+    expect(1).toBe(1);
+  });
+  
+  it('test two', () => {
+    expect(1).toBe(1);
+  });
+
+  it('test three', () => {
+    expect(1).toBe(1);
+  });
+
+});
+
+
+
+
+
+
